eb5f173e78d23f33178b1b352d66b43bea88cb69
MrWings22 <albinthomas6210@gmail.com>
2025-02-23 18:20:54 +0530

    fetching news

--- news_project/newsapp/urls.py
+++ news_project/newsapp/urls.py
@@ -1,16 +1,8 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hhj', views.Indexpage,  name="Indexpage"),
+    path('jj', views.Indexpage,  name="Indexpage"),
     path('', views.Homepage,  name="Homepage"),
-    path('jk', views.Detailedpage,  name="Detailedpage"),
-<<<<<<< HEAD
+    path('kk', views.Detailedpage,  name="Detailedpage"),
 ]
-=======
-    path('img/', views.NewsImages, name='NewsImages'),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
->>>>>>> 2fa29a04061c204eafa06d547bce4678ed9f939b
--- news_project/newsapp/views.py
+++ news_project/newsapp/views.py
@@ -5,13 +5,12 @@ def Indexpage(request):
     return render(request, "index.html")
 
 def Homepage(request):
-    latestnews = Article.objects.all()
-    return render(request, 'home.html', {'latestnews': latestnews})
-
+    latestnews = Article.objects.order_by('-created_at').first()
+    topfivenews = Article.objects.order_by('-created_at')[:5]
+    return render(request, 'home.html', {'latestnews': latestnews ,'topfivenews': topfivenews})
+    
 def Detailedpage(request):
     return render(request, "detail-page.html")
 
-def NewsImages(request):
-    images = ArticleImages.objects.all()
-    return render(request, 'home.html', {'images': images})
+
     

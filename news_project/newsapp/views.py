333bfd4bc0d5caf0f3c97825d7b8a98b0a64d6c1
MrWings22 <albinthomas6210@gmail.com>
2025-03-03 13:56:29 +0530

    added search css

--- news_project/newsapp/views.py
+++ news_project/newsapp/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from .models import Article, ArticleImages
 
 def Indexpage(request):
+
     return render(request, "index.html")
 
 def Homepage(request):
     latestnews = Article.objects.order_by('-created_at').first()
     topfivenews = Article.objects.order_by('-created_at').exclude(pk=latestnews.pk)[:5]
     latestnews_images = ArticleImages.objects.filter(article=latestnews) if latestnews else None
+    # othernews = Article.objects.order_by('-created_at').exclude(pk=topfivenews.pk)[:5]
     return render(request, 'home.html', {'latestnews': latestnews ,'latestnews_images': latestnews_images,'topfivenews': topfivenews})
 
 def articledetail(request, article_id):
